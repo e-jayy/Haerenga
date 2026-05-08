49c98ce48ccc3278a8eff7425894a42b38821e71
ejay <et4g23@soton.ac.uk>
2026-05-08 01:31:34 +0100

    Fishhook collect UI + Sand Tileset + Font implementation
    Add a new Tile_Sand palette including prefab, PNG spritesheet, sprite/meta and tile assets for sand tiles. Add Meadow Bloom font (OTF) and generated SDF atlas asset. Move font files out of '2D Assets/Fonts' into 'Assets/Fonts' and relocate several scripts (DisableCutsceneCamera, FishhookFragment, PlayNPCDialogeTimeline) into 'Assets/Scripts'. Remove the old root FishhookFragment.cs. Update Menu prefab and Level_Village scene to reflect these asset changes.

---
+++ Haerenga/Assets/Scripts/FishhookFragment.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class FishhookFragment : MonoBehaviour
+{
+    [SerializeField] private GameObject abilityInfoPanel;
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private bool hasBeenCollected = false;
+    [SerializeField] private bool collectHook;
+    [SerializeField] private bool collectWallJump;
+    private bool isPaused;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OpenPage();
+            UnlockAbility();
+        }
+    }
+    public void Update ()
+    {
+        if(InputManager.instance.JumpJustPressed && abilityInfoPanel.activeSelf)
+        {
+            ClosePage();
+        }
+    }
+
+    private void OpenPage()
+    {
+        abilityInfoPanel.SetActive(true);
+        Pause();
+        hasBeenCollected = true;
+    }  
+    public void ClosePage()
+    {
+        abilityInfoPanel.SetActive(false);
+        Unpause();
+    }
+
+        public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+
+        if(_playerController != null)
+        {
+        _playerController.enabled = false;    
+        }
+    }
+
+    public void Unpause()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+
+        if(_playerController != null)
+        {
+        _playerController.enabled = true;    
+        }
+        
+        Destroy(gameObject);
+    }
+
+    public void UnlockAbility()
+    {
+        if(collectHook)
+        {
+            Debug.Log("Unlocking Hook Ability");
+            PlayerManager.Instance.UnlockHook();
+        }
+        else if(collectWallJump)
+        {   
+            Debug.Log("Unlocking Wall Jump Ability");
+            PlayerManager.Instance.UnlockWallJump();
+        }
+    }
+}
+
