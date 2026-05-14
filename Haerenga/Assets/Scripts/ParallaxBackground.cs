4b461c5eafe6edc10a27a38dedf89e567273b343
ejay <et4g23@soton.ac.uk>
2026-05-14 17:29:25 +0100

    Fixed bug where Level 2 Background would have weird offset

--- Haerenga/Assets/Scripts/ParallaxBackground.cs
+++ Haerenga/Assets/Scripts/ParallaxBackground.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
     [Header("Parallax Settings")]
-    [SerializeField] [Range(0f, 1f)] private float parallaxX = 0.1f; // Lower = less movement
-    [Range(0f, 1f)] [SerializeField] private float parallaxY = 0.05f; // Set to 0 to lock vertical
+    [SerializeField] [Range(0f, 1f)] private float parallaxX = 0.1f;
+    [SerializeField] [Range(0f, 1f)] private float parallaxY = 0.05f;
     [SerializeField] private bool lockVertical = false;
 
     private Transform cam;
     private Vector3 lastCamPos;
+    private Vector3 startOffset; // Offset from camera at scene start
 
     void Start()
     {
         cam = Camera.main.transform;
         lastCamPos = cam.position;
+        
+        // Store the initial offset between background and camera
+        startOffset = transform.position - cam.position;
     }
 
     void LateUpdate()
