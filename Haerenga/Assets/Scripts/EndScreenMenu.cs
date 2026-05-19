ccb73bdffe86196323524d9019a5cd87ace8e13d
ejay <et4g23@soton.ac.uk>
2026-05-19 19:51:08 +0100

    Added controller support to end scene menu

--- Haerenga/Assets/Scripts/EndScreenMenu.cs
+++ Haerenga/Assets/Scripts/EndScreenMenu.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EndScreenMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _endMenuFirst;
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(_endMenuFirst);
+    }
     public void OnStartPress()
     {
         SceneController.Instance.ResetSpawnData();
